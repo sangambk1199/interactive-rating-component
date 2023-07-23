a4500eb1ed528db782e799fb0f4d1b61e69bdba9
sangam <sangambk1199@gmail.com>
2023-07-23 19:30:00 +0545

    Deploy Test

--- src/components/NumericRating.tsx
+++ src/components/NumericRating.tsx
@@ -1,16 +1,16 @@
 type NumericRatingProps = {
-    ratingScore: number,
+    rating: number,
     onRatingChange: (rating: number) => void
 }
 
-export default function NumericRating({ratingScore, onRatingChange}: NumericRatingProps) {
+export default function NumericRating({rating, onRatingChange}: NumericRatingProps) {
     return (
         <div>
-            {/* onChange={() => onRatingChange(ratingScore)} */}
-            <input type="radio" className="hidden peer" id={ 'rating-' + ratingScore } name="numeric-rating"  
-                value={ratingScore} onChange={() => onRatingChange(ratingScore)} />
-            <label htmlFor={ 'rating-' + ratingScore } className="block p-3 bg-gray-700 opacity-30 aspect-square rounded-full flex-shrink-0 w-12 h-12 text-center cursor-pointer hover:bg-orange-500 hover:opacity-100 peer-checked:opacity-100">
-                {ratingScore}
+            {/* onChange={() => onRatingChange(rating)} */}
+            <input type="radio" className="hidden peer" id={ 'rating-' + rating } name="numeric-rating"  
+                value={rating} onChange={() => onRatingChange(rating)} />
+            <label htmlFor={ 'rating-' + rating } className="block p-3 bg-gray-700 opacity-30 aspect-square rounded-full flex-shrink-0 w-12 h-12 text-center cursor-pointer hover:bg-orange-500 hover:opacity-100 peer-checked:opacity-100">
+                {rating}
             </label>
         </div>
     )
