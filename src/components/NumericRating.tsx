bb2a2c2b79047456724576d329794d3ff37f520c
sangam <sangambk1199@gmail.com>
2023-07-23 19:08:29 +0545

    Function removed

--- src/components/NumericRating.tsx
+++ src/components/NumericRating.tsx
@@ -1,12 +1,13 @@
 type NumericRatingProps = {
     ratingScore: number,
-    onRatingChange: (rating: number) => void
+    //onRatingChange: (rating: number) => void
 }
 
-export default function NumericRating({ratingScore, onRatingChange}: NumericRatingProps) {
+export default function NumericRating({ratingScore}: NumericRatingProps) {
     return (
         <div>
-            <input type="radio" className="hidden peer" id={ 'rating-' + ratingScore } name="numeric-rating" onChange={() => onRatingChange(ratingScore)} value={ratingScore}/>
+            {/* onChange={() => onRatingChange(ratingScore)} */}
+            <input type="radio" className="hidden peer" id={ 'rating-' + ratingScore } name="numeric-rating"  value={ratingScore}/>
             <label htmlFor={ 'rating-' + ratingScore } className="block p-3 bg-gray-700 opacity-30 aspect-square rounded-full flex-shrink-0 w-12 h-12 text-center cursor-pointer hover:bg-orange-500 hover:opacity-100 peer-checked:opacity-100">
                 {ratingScore}
             </label>
